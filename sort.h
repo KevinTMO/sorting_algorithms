0d74ae319f57f8306538f63e57278622980e0839
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-26 22:28:13 +0000

    Updated

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void print_array(const int *array, size_t size);
 /* TASK FUNCTIONS BELOW */
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
